41780d8c18c6674581efa2cab67ad390336467dc
ortizjulian <julian.ortixs@gmail.com>
2024-03-20 16:43:23 -0500

    Fix gallery image functionality

--- dev/js/producto_compra.js
+++ dev/js/producto_compra.js
@@ -6,22 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     let indiceFoto = 0;
-    let indiceFotoCrear = 0;
     const fotos = [fotoProducto1, fotoProducto2];
+    const puntos = document.querySelectorAll('.punto-comprador');
+
+    let indiceFotoCrear = 0;
     const fotosCrear = [fotoCrear1, fotoCrear2];
+    const puntosCrear = document.querySelectorAll('.punto-crear');
+
+    let indiceFotoDetalles = 0;
+    const puntosDetalles = document.querySelectorAll('.punto-detalles');
+
     const interruptor = document.getElementById("interruptor");
     const estadoInterruptor = document.getElementById("estado-interruptor");
     const categorias1 = document.getElementById("categorias-1");
     const categorias2 = document.getElementById("categorias-2");
-    const puntos = document.querySelectorAll('.punto');
 
-    function cambiarFoto(direccion) {
+
+    function cambiarFotoComprador(direccion) {
+        console.log(puntos)
         if (direccion === 'izquierda') {
             indiceFoto = (indiceFoto - 1 + fotos.length) % fotos.length;
         } else if (direccion === 'derecha') {
             indiceFoto = (indiceFoto + 1) % fotos.length;
         }
-        document.querySelector('.foto_producto').src = fotos[indiceFoto];
+        document.getElementById('foto_producto').src = fotos[indiceFoto];
 
         puntos.forEach((punto, index) => {
             if (index === indiceFoto) {
@@ -38,7 +46,32 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (direccion === 'derecha') {
             indiceFotoCrear = (indiceFotoCrear + 1) % fotosCrear.length;
         }
-        document.querySelector('.foto_producto_crear').src = fotosCrear[indiceFotoCrear];
+        document.getElementById('foto_producto_crear').src = fotosCrear[indiceFotoCrear];
+
+        puntosCrear.forEach((punto, index) => {
+            if (index === indiceFotoCrear) {
+                punto.classList.add('seleccionado');
+            } else {
+                punto.classList.remove('seleccionado');
+            }
+        });
+    }
+
+    function cambiarFotoDetalles(direccion) {
+        if (direccion === 'izquierda') {
+            indiceFotoDetalles = (indiceFotoDetalles - 1 + fotosCrear.length) % fotosCrear.length;
+        } else if (direccion === 'derecha') {
+            indiceFotoDetalles = (indiceFotoDetalles + 1) % fotosCrear.length;
+        }
+        document.getElementById('foto_producto_detalles').src = fotosCrear[indiceFotoDetalles];
+
+        puntosDetalles.forEach((punto, index) => {
+            if (index === indiceFotoDetalles) {
+                punto.classList.add('seleccionado');
+            } else {
+                punto.classList.remove('seleccionado');
+            }
+        });
     }
 
 
@@ -56,35 +89,31 @@ document.addEventListener("DOMContentLoaded", function () {
             estadoInterruptor.classList.remove('switch-desactivado');
         }
     });
-
+    //Galeria detalles compra
     document.getElementById('flecha_izquierda').addEventListener('click', function () {
-        cambiarFoto('izquierda');
-        actualizarPuntos();
+        cambiarFotoComprador('izquierda');
     });
 
     document.getElementById('flecha_derecha').addEventListener('click', function () {
-        cambiarFoto('derecha');
-        actualizarPuntos();
+        cambiarFotoComprador('derecha');
     });
-
+    //Galeria crear
     document.getElementById('flecha_izquierda_crear').addEventListener('click', function () {
         cambiarFotoCrear('izquierda');
-        actualizarPuntos();
+
     });
 
     document.getElementById('flecha_derecha_crear').addEventListener('click', function () {
         cambiarFotoCrear('derecha');
-        actualizarPuntos();
-    });
 
+    });
+    //Galeria detalles
     document.getElementById('flecha_izquierda_detalles').addEventListener('click', function () {
-        cambiarFotoCrear('izquierda');
-        actualizarPuntos();
+        cambiarFotoDetalles('izquierda');
     });
 
     document.getElementById('flecha_derecha_detalles').addEventListener('click', function () {
-        cambiarFotoCrear('derecha');
-        actualizarPuntos();
+        cambiarFotoDetalles('derecha');
     });
 
     document.getElementById('cambiar-categoria-izquierda').addEventListener('click', function () {
