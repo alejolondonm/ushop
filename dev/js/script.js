8a4573851d0b683ca340384cb844634f01f42087
ortizjulian <julian.ortixs@gmail.com>
2024-03-19 23:57:38 -0500

    join login views and buyer views

---
+++ dev/js/script.js
@@ -0,0 +1,11 @@
+function cambiarSeccion(idOcultar, idMostrar) {
+    const seccionOcultar = document.getElementById(idOcultar);
+    const seccionMostrar = document.getElementById(idMostrar);
+
+    if (seccionOcultar && seccionMostrar) {
+        seccionOcultar.classList.add('ocultar');
+        seccionMostrar.classList.remove('ocultar');
+    } else {
+        console.error('Una o ambas secciones especificadas no existen.');
+    }
+}
